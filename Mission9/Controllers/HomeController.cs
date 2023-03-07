f335a1df4ee3728917e878942f56af5d0d80d8b8
Kate Opager <opagerk@byu.edu>
2023-03-06 22:36:47 -0700

    .

--- Mission9/Controllers/HomeController.cs
+++ Mission9/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Mission9.Controllers
             repo = temp;
         }
 
-        public IActionResult Index(int pageNum = 1)
+        public IActionResult Index(string bookCategory, int pageNum = 1)
         {
 
             int pageSize = 10;
@@ -25,6 +25,7 @@ namespace Mission9.Controllers
             var x = new BooksViewModel
             {
                 Books = repo.Books
+                .Where( b => b.Category)
                 .OrderBy(b => b.Title)
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize),
