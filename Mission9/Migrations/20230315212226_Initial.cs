ea3412692aae0b05674eb89b0e20a5817106d71d
Kate Opager <opagerk@byu.edu>
2023-03-17 12:56:36 -0600

    .

--- Mission9/Migrations/20230315212226_Initial.cs
+++ Mission9/Migrations/20230315212226_Initial.cs
@@ -6,25 +6,25 @@ namespace Mission9.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Books",
-                columns: table => new
-                {
-                    BookId = table.Column<long>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Title = table.Column<string>(nullable: true),
-                    Author = table.Column<string>(nullable: true),
-                    Publisher = table.Column<string>(nullable: true),
-                    Isbn = table.Column<string>(nullable: true),
-                    Classification = table.Column<string>(nullable: true),
-                    Category = table.Column<string>(nullable: true),
-                    PageCount = table.Column<long>(nullable: false),
-                    Price = table.Column<double>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Books", x => x.BookId);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Books",
+            //    columns: table => new
+            //    {
+            //        BookId = table.Column<long>(nullable: false)
+            //            .Annotation("Sqlite:Autoincrement", true),
+            //        Title = table.Column<string>(nullable: true),
+            //        Author = table.Column<string>(nullable: true),
+            //        Publisher = table.Column<string>(nullable: true),
+            //        Isbn = table.Column<string>(nullable: true),
+            //        Classification = table.Column<string>(nullable: true),
+            //        Category = table.Column<string>(nullable: true),
+            //        PageCount = table.Column<long>(nullable: false),
+            //        Price = table.Column<double>(nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Books", x => x.BookId);
+            //    });
 
             migrationBuilder.CreateTable(
                 name: "Purchase",
