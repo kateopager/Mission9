7327e9e3369e23e34acc6d6a6df919e6103180a0
Kate Opager <opagerk@byu.edu>
2023-03-17 14:31:59 -0600

    .

---
+++ Mission9/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission9.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketservice)
+        {
+            basket = basketservice;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+        
+    }
+}
